a73c9dc6cb27668b1fa47a911ae2854ffe77cf6e
amit verma <ribweb32@ROBINHOODINSURANCE.IN>
2019-05-17 17:43:17 +0530

    new update

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 import '../scss/main.scss';
-import '../vendors/jquery/jquery.min.js';
 import '../vendors/jquery/popper.min.js';
 import '../vendors/bootstrap/bootstrap.min.js'; 
 import './main.js';
--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
 // document ready
-// $(document).ready(function(){
-//     console.log('ready now');
-// });
+
 // document ready end
