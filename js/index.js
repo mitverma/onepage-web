af8262e7ae236f155f3fc418f6b380d5ed919ade
amit verma <ribweb32@ROBINHOODINSURANCE.IN>
2019-05-17 16:23:05 +0530

    first wala commit mann

---
+++ js/index.js
@@ -0,0 +1,5 @@
+import '../scss/main.scss';
+import '../vendors/jquery/jquery.min.js';
+import '../vendors/jquery/popper.min.js';
+import '../vendors/bootstrap/bootstrap.min.js'; 
+import './main.js';
---
+++ js/main.js
@@ -0,0 +1,5 @@
+// document ready
+// $(document).ready(function(){
+//     console.log('ready now');
+// });
+// document ready end
