52b4b216f626b0c0a546cb5a2bc910a79a09f6e1
amit verma <ribweb32@ROBINHOODINSURANCE.IN>
2019-05-20 15:28:23 +0530

    changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 // document ready
 $(document).ready(()=>{
     $('main .auto-height').css('height', $(window).height());
+
+
+    // scroll to 
+    $('.scrollTo').click(()=>{
+        $('.checkbox-toggle')[0].checked = false;
+    });
+    // scroll to end
 })
 // document ready end
